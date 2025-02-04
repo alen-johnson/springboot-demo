bde460c613dce579a1af60c3b9f959fb81c306d2
Alen J Ninan <alen.j@captainfresh.in>
2025-02-04 15:21:43 +0530

    added .env

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+		System.setProperty("MONGO_DB", dotenv.get("MONGO_DB"));
+
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("demo start");
 	}
